54d5c980eb1e9064b8e4abc89a11b7bc9369e2f0
io-kobayashiii <io.kobayashiii@gmail.com>
2024-07-14 21:14:16 +0900

    Add env files

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log('process.env.LISTEN_PORT', process.env.LISTEN_PORT);
+  await app.listen(process.env.LISTEN_PORT ?? 3000);
 }
 bootstrap();
+
+// test
